e8d6a09073a438c910d74a3486387885db70f11e
Vignesh <vignesh.bsk@gmail.com>
2024-09-12 23:22:21 +0530

    Added insert query with error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,json,request
 import psycopg2
 
 app=Flask(__name__)
@@ -9,7 +9,7 @@ def connection():
     
 @app.route('/')
 def test():
-    return render_template('login.html')
+    return render_template('home.html')
 
 @app.route('/form')
 def form():
@@ -17,15 +17,41 @@ def form():
 
 @app.route('/create',methods=['POST'])
 def dbcon():
-    query=f'''CREATE TABLE IF NOT EXISTS "{table_name}" (RollNumber VARCHAR(255),Name VARCHAR(255), Year INT); '''
+    RollNumber = request.form['Roll Number']
+    Name=request.form['Name']
+    TamilMark=request.form['Tamil Mark']
+    EnglishMark=request.form['English Mark']
+    MathsMark=request.form['Maths Mark']
+    ScienceMark=request.form['Science Mark']
+    SocialMark=request.form['Social Mark']
+    table_name=request.form['Select Test']
+
+    CreateQuery=f'''CREATE TABLE IF NOT EXISTS "{table_name}" (
+    "Roll Number" VARCHAR(20) PRIMARY KEY,
+    "Name" VARCHAR(100) NOT NULL,
+    "Tamil Mark" INTEGER NOT NULL,
+    "English Mark" INTEGER NOT NULL,
+    "Maths Mark" INTEGER NOT NULL,
+    "Science Mark" INTEGER NOT NULL,
+    "Social Mark" INTEGER NOT NULL
+); '''
     con=connection()
     cur=con.cursor()
-    cur.execute(query)
+    cur.execute(CreateQuery)
     con.commit()
+
+    query="""INSERT INTO "Unit Test 1" ("Roll Number", "Name", "Tamil Mark", "English Mark", "Maths Mark", "Science Mark", "Social Mark") VALUES ('1', 'Vignesh B', '60', '70', '70', '80', '68')"""
+    InsertQuery=(f"""INSERT INTO "{table_name}" ("Roll Number", "Name", "Tamil Mark", "English Mark", "Maths Mark", "Science Mark", "Social Mark") VALUES (%s, %s, %s, %s, %s, %s, %s)""",(RollNumber,Name,TamilMark,EnglishMark,MathsMark,ScienceMark,SocialMark))
+    vars=(RollNumber,Name,TamilMark,EnglishMark,MathsMark,ScienceMark,SocialMark)
+    #cursor.execute("""INSERT INTO my_data (name, age, score1, score2, score3, score4, score5) VALUES (%s, %s, %s, %s, %s, %s, %s) """, (name, age, score1, score2, score3, score4, score5))
+    print(query)
+    cur.execute(InsertQuery)
+    con.commit()
+
     cur.close()
     con.close()
 
-    return "Successful"
+    return render_template('Success.html')
 
 if __name__== '__main__':
     app.run(debug=True)
